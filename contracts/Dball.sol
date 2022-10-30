c438f02c15f62354092bc36ae3b98e665d43750f
Yuki Takahashi <yuki0627@gmail.com>
2022-10-30 11:09:23 +0900

    Get the number of types of target NFTs you own

--- contracts/Dball.sol
+++ contracts/Dball.sol
@@ -6,28 +6,37 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
 
 interface ERC721Contract {
-    // function balanceOf(address owner) external view returns (uint256);
-    // function totalSupply() external view returns (uint256);
+    function balanceOf(address) external view returns (uint256);
+    function totalSupply() external view returns (uint256);
 }
 
 contract Dball is Ownable, ERC721 {
-    address[7] _targetContracts;
+    address[7] _targetContractaAddresses;
 
     constructor() ERC721("D-BALL", "DBL") {
     }
 
-    function addTargetContract(uint8 index, address _contractAddress) public onlyOwner {
-        console.log("index:", index);
-        console.log("_contractAddress:", _contractAddress);
-
-        _targetContracts[index] = _contractAddress;
+    function setTargetContract(uint8 index, address _contractAddress) public onlyOwner {
+        _targetContractaAddresses[index] = _contractAddress;
     }
 
-    function getTargetContracts() external view returns(address[7] memory) {
+    function getTargetContracts() public view returns(address[7] memory) {
         address[7] memory res;
-        for(uint16 i = 0; i < 7; i++){
-            res[i] = _targetContracts[i];
+        for(uint8 i = 0; i < 7; i++){
+            res[i] = _targetContractaAddresses[i];
         }
         return res;
+    }
+
+    function getNumberOfBalls(address target_account_address) public view returns(uint256) {
+        console.log("_targetContractaAddresses[0]:", _targetContractaAddresses[0]);
+        console.log("target_account_address: ", target_account_address);
+
+        uint256 total;
+        for(uint8 i = 0; i < 7; i++){
+            ERC721Contract erc721 = ERC721Contract(_targetContractaAddresses[i]);
+            total += erc721.balanceOf(target_account_address);
+        }
+        return total;
     }
 }
--- contracts/DummyNFT.sol
+++ contracts/DummyNFT.sol
@@ -2,8 +2,18 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract DummyNFT is ERC721 {
-    constructor() ERC721("D-BALL", "DBL") {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("Dummy", "DUM") {
+    }
+    
+    function nftMint() public {
+         _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+        _mint(msg.sender, newTokenId);
     }
 }
