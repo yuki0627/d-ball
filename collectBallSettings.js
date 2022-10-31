34dbbfa67cb3cb6824a96b24c29c217904267b82
Yuki Takahashi <yuki0627@gmail.com>
2022-10-31 21:40:49 +0900

    Partially deployed for verification

---
+++ collectBallSettings.js
@@ -0,0 +1,6 @@
+module.exports = {
+            contractName: 'tmp Collect Balls',
+            shortName: 'TCB',
+            targetAddresses: ["0x19F048D1614cba75C1CfFC191D92b03cBB6150Df"]
+        }
+        
