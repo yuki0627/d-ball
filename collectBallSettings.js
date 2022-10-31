e625a1fbb0a29d92783f9669322f7217cdfd3c5c
Yuki Takahashi <yuki0627@gmail.com>
2022-11-01 05:57:09 +0900

    set deployed address

--- collectBallSettings.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-            contractName: 'tmp Collect Balls',
-            shortName: 'TCB',
-            targetAddresses: ["0x19F048D1614cba75C1CfFC191D92b03cBB6150Df"]
-        }
-        
