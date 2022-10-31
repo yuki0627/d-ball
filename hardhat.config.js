d3d63347da1f7e49a977f992bfc2757f03641ce5
Yuki Takahashi <yuki0627@gmail.com>
2022-10-31 20:07:29 +0900

    Deployment coordination

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-gas-reporter");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY],
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,41 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const fs = require("fs");
+const main = async () => {
+    addressList = [];
+    ContractCount = 1; // 1-7 いくつの種類のボールをデプロイするか
+    NftCount = 3; // 0~ ボールの種類ごと、いくつNFTをmintしておくか
+    // デプロイ
+    for (i = 0; i < ContractCount; i++){
+        Ball = await ethers.getContractFactory("Ball");
+        contract = await Ball.deploy(`TEST-D-BALL-${i+1}`, `TDB${i+1}`);
+        await contract.deployed();
+        console.log(`contract${i} deployed to: https://goerli.etherscan.io/address/${contract.address}`);
+        addressList.push(contract.address);
+        for (n = 0; n < NftCount; n++){
+            tx = await contract.nftMint(`D-BALL-${i + 1}`, `${n + 1}/${NftCount}`);
+            await tx.wait();
+            console.log('NFT minted:', `BALL-${i + 1}: NFT-${n + 1}`);
+        }
+    }
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+    // コントラクトアドレスの書き出し
+    fs.writeFileSync(`./collectBallSettings.js`,
+        `module.exports = {
+            contractName: 'tmp Collect Balls',
+            shortName: 'TCB',
+            targetAddresses: ${JSON. stringify(addressList)}
+        }
+        `
+    );
+};
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+const deploy = async () => { 
+    try {
+        await main();
+        process.exit(0);
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+};
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
-}
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+deploy();
